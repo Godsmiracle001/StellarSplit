39fc16b91761d8a38cc6efb659cd5bae0d599550
“Peteroche” <petergoddey08l@gmail.com>
2026-01-22 17:07:27 +0100

    Create Item Entity for Itemized Splits

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,6 +10,7 @@ import { StellarModule } from './stellar/stellar.module';
 import { PaymentsModule } from './payments/payments.module';
 import { CurrencyModule } from './modules/currency/currency.module';
 import { SplitsModule } from './modules/splits/splits.module';
+import { ItemsModule } from './modules/items/items.module';
 
 // Load environment variables
 dotenv.config({
@@ -45,6 +46,7 @@ dotenv.config({
     PaymentsModule,
     CurrencyModule,
     SplitsModule,
+    ItemsModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
